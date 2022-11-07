b0fecf8bb3effde46388ee814df6f5fcb6b0bab9
Andrew Richards <a.richards@computer.org>
2022-11-07 10:11:58 +0000

    Updated my layout to candybar

--- keyboards/tkc/candybar/lefty/keymaps/default/keymap.c
+++ keyboards/tkc/candybar/lefty/keymaps/default/keymap.c
@@ -16,9 +16,9 @@
 
 #include QMK_KEYBOARD_H
 
-#define _BL 1
-#define _ALPHA 0
-#define _FN 2
+#define _BL 0
+#define _ALPHA 1
+#define _FUNCTION 2
 #define _FD 3
 #define _ASYMBOLS 4
 #define _SYMBOLS 5
@@ -38,34 +38,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   // This base layer has cursor keys on the right and 2 function keys
 [_BL] = LAYOUT(
-  KC_P7 , KC_P8,  KC_P9  ,KC_MINS,        KC_ESC ,            KC_Q   , KC_W  , KC_E, KC_R  , KC_T, KC_Y, KC_U, KC_I   , KC_O  , KC_P   ,KC_BSPC,         KC_RBSP , \
-  KC_P4 , KC_P5,  KC_P6  ,REVPLUS,        MT(MOD_LCTL,KC_TAB),KC_A   , KC_S  , KC_D, KC_F  , KC_G, KC_H, KC_J, KC_K   , KC_L  , KC_SCLN,                  KC_ENT  , \
-  KC_P1 , KC_P2,  KC_P3  ,LT(_FD,KC_NUBS),KC_LSFT,       KC_Z   , KC_X,   KC_C, KC_V  , KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP  ,      MT(MOD_RSFT,KC_SLSH), \
-  KC_P0,  KC_P0,  KC_PDOT,LT(_SYMBOLS,KC_PENT),LT(_FN,KC_NUHS),KC_LGUI,KC_LALT,KC_SPC,LT(_SYMBOLS,KC_SPC),KC_SPC, LT(_SYMBOLS,KC_QUOTE),KC_LEFT, KC_DOWN, KC_RGHT),
+  KC_P7 , KC_P8,  KC_P9  ,KC_MINS,  KC_ESC ,            KC_Q   , KC_W  , KC_E, KC_R  , KC_T, KC_Y, KC_U, KC_I   , KC_O  , KC_P   ,KC_NUBS,         KC_RBSP , \
+  KC_P4 , KC_P5,  KC_P6  ,REVPLUS,  MT(MOD_LCTL,KC_TAB),KC_A   , KC_S  , KC_D, KC_F  , KC_G, KC_H, KC_J, KC_K   , KC_L  , KC_SCLN,                  KC_ENT  , \
+  KC_P1 , KC_P2,  KC_P3  ,STARPSCR, KC_LSFT,            KC_Z   , KC_X,   KC_C, KC_V  , KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP  ,      LT(_FD,KC_SLSH), \
+  KC_P0,  KC_P0,  KC_PDOT,KC_PENT,  LT(_FUNCTION,KC_NUHS),KC_LGUI,KC_LALT,KC_SPC,LT(_SYMBOLS,KC_SPC),KC_SPC, LT(_SYMBOLS,KC_QUOTE),KC_LEFT, KC_DOWN, KC_RGHT),
   
   /* Keymap _ALPHA: Alpha Layer (no cursor keys on the right)
   */
 [_ALPHA] = LAYOUT(
-  KC_P7 , KC_P8,  KC_P9  ,KC_MINS,        KC_ESC ,            KC_Q   , KC_W  , KC_E, KC_R  , KC_T, KC_Y, KC_U, KC_I   , KC_O  , KC_P   ,KC_BSPC,         KC_RBSP , \
-  KC_P4 , KC_P5,  KC_P6  ,REVPLUS,        MT(MOD_LCTL,KC_TAB),KC_A   , KC_S  , KC_D, KC_F  , KC_G, KC_H, KC_J, KC_K   , KC_L  , KC_SCLN,                  KC_ENT  , \
-  KC_P1 , KC_P2,  KC_P3  ,LT(_FD,KC_NUBS),KC_LSFT,            KC_Z   , KC_X,   KC_C, KC_V  , KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH  ,      KC_RSHIFT,          \
-  KC_P0,  KC_P0,  KC_PDOT,LT(_ASYMBOLS,KC_PENT),LT(_FN,KC_NUHS),KC_LGUI,KC_LALT,KC_SPC,LT(_ASYMBOLS,KC_SPC),KC_SPC, LT(_ASYMBOLS,KC_QUOTE),MT(MOD_RALT,KC_LBRC),LT(_SYMBOLS,KC_QUOTE/*KC_APP*/),MT(MOD_RCTL,KC_RBRC)),
+  KC_P7 , KC_P8,  KC_P9  ,KC_MINS,  KC_ESC ,            KC_Q   , KC_W  , KC_E, KC_R  , KC_T, KC_Y, KC_U, KC_I   , KC_O  , KC_P   ,KC_NUBS,         KC_RBSP , \
+  KC_P4 , KC_P5,  KC_P6  ,REVPLUS,  MT(MOD_LCTL,KC_TAB),KC_A   , KC_S  , KC_D, KC_F  , KC_G, KC_H, KC_J, KC_K   , KC_L  , KC_SCLN,                  KC_ENT  , \
+  KC_P1 , KC_P2,  KC_P3  ,STARPSCR, KC_LSFT,            KC_Z   , KC_X,   KC_C, KC_V  , KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH  ,      LT(_FD,KC_SLSH),          \
+  KC_P0,  KC_P0,  KC_PDOT,KC_PENT,  LT(_FUNCTION,KC_NUHS),KC_LGUI,KC_LALT,KC_SPC,LT(_ASYMBOLS,KC_SPC),KC_SPC, LT(_ASYMBOLS,KC_QUOTE),MT(MOD_RALT,KC_LBRC),LT(_SYMBOLS,KC_QUOTE/*KC_APP*/),MT(MOD_RCTL,KC_RBRC)),
 
   /* Keymap _FD: Cursor keys on pad and mouse
   */
 [_FD] = LAYOUT(
-  KC_HOME, KC_UP,  KC_PGUP, KC_PMNS,KC_GRV,  S(KC_1) ,S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), S(KC_MINS),S(KC_EQL), \
-  KC_LEFT, KC_F5,  KC_RIGHT,KC_PPLS,KC_LCTL, KC_WH_U, KC_SLCK, TO(_BL), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_QUOT,          KC_PENT , \
-  KC_END,  KC_DOWN,KC_PGDN, KC_CALC,KC_LSFT, KC_WH_D, XXXXXXX, KC_CALC, XXXXXXX, XXXXXXX, KC_NLCK, XXXXXXX, XXXXXXX, KC_PDOT, KC_MS_U,   KC_RSFT , \
-  KC_INS,  KC_INS, KC_DEL,  KC_CALC,_______,KC_APP,  KC_LALT,                KC_MS_BTN1,     KC_MS_BTN1 , KC_MS_BTN2,  KC_RALT, KC_MS_L, KC_MS_D, KC_MS_R),
+  KC_F7 , KC_F8,  KC_F9 , KC_F11,  KC_GRV,  S(KC_1) ,S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), S(KC_MINS),S(KC_EQL), \
+  KC_F4 , KC_F5,  KC_F6 , KC_F12,  KC_LCTL, KC_WH_U, KC_SLCK, TO(_BL), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_QUOT,          KC_BRK , \
+  KC_F1 , KC_F2,  KC_F3 , KC_PSCR, KC_LSFT, KC_WH_D, XXXXXXX, KC_CALC, XXXXXXX, XXXXXXX, KC_NLCK, XXXXXXX, XXXXXXX, KC_PDOT, KC_MS_U,   KC_RSFT , \
+  KC_F10, KC_F10, KC_BRK, KC_SLCK, _______, KC_APP,  KC_LALT,                KC_MS_BTN1,     KC_MS_BTN1 , KC_MS_BTN2,KC_RALT, KC_MS_L, KC_MS_D, KC_MS_R),
 
-  /* Keymap _FN: Function keys Layer
+  /* Keymap _FUNCTION: Function keys Layer
   */
-[_FN] = LAYOUT(
-  KC_F7 , KC_F8,  KC_F9 , KC_F11, KC_GRV  ,XXXXXXX,XXXXXXX,TO(_BL),RESET,XXXXXXX,XXXXXXX,XXXXXXX,KC_INS, XXXXXXX,KC_PSCR,KC_DEL,KC_DEL , \
-  KC_F4 , KC_F5,  KC_F6 , KC_F12,MT(MOD_LCTL,KC_CAPS),TO(_ALPHA), KC_SLCK, TO(_BL), XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX,XXXXXXX, XXXXXXX, KC_QUOTE, KC_NUHS, \
-  KC_F1 , KC_F2,  KC_F3 , XXXXXXX, KC_LSFT,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_VAI,  KC_PAUS, KC_NLCK, XXXXXXX,XXXXXXX, XXXXXXX, KC_PGUP, KC_RSFT , \
-  KC_F10, KC_F10, XXXXXXX, _______, _______, KC_APP,  KC_LALT,                KC_SPC,       KC_SPC , KC_SPC,         OSM(MOD_RALT), KC_HOME,  KC_PGDN, KC_END),
+[_FUNCTION] = LAYOUT(
+  KC_HOME, KC_UP,  KC_PGUP, KC_PMNS,KC_GRV  ,XXXXXXX,XXXXXXX,TO(_BL),RESET,XXXXXXX,XXXXXXX,XXXXXXX,KC_INS, KC_POWER,KC_PSCR,KC_DEL,KC_DEL , \
+  KC_LEFT, XXXXXXX,KC_RIGHT,KC_PPLS,MT(MOD_LCTL,KC_CAPS),TO(_ALPHA), KC_SLCK, TO(_BL), XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX,XXXXXXX, XXXXXXX, KC_QUOTE, KC_BRK, \
+  KC_END,  KC_DOWN,KC_PGDN, KC_CALC,KC_LSFT,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_VAI,  KC_PAUS, KC_NLCK, XXXXXXX,XXXXXXX, XXXXXXX, KC_PGUP, KC_RSFT , \
+  KC_INS,  KC_INS, KC_DEL,  KC_CALC,_______, KC_APP,  KC_LALT,                KC_SPC,       KC_SPC , KC_SPC,         OSM(MOD_RALT), KC_HOME,  KC_PGDN, KC_END),
 
   /* Keymap _SYMBOLS: Symbols Layer (holding right FN)
   */
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   S(KC_7) , S(KC_8),  S(KC_9),   KC_PSLS, KC_MINS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, \
   S(KC_4) , S(KC_5),  S(KC_6),   KC_PAST, S(KC_MINS),S(KC_1),S(KC_2),S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), S(KC_NUHS), \
   S(KC_1) , S(KC_2),  S(KC_3)  , KC_CALC, KC_LSFT, KC_NUBS, XXXXXXX, XXXXXXX,RALT(KC_4),KC_SCLN,KC_QUOTE, KC_NUHS, KC_LBRC, KC_RBRC, KC_SLSH, KC_RSFT , \
-  S(KC_0), S(KC_0),   S(KC_SLSH),KC_CALC, KC_LCTL, KC_APP,  KC_LALT,                KC_SPC,  KC_SPC , KC_SPC,_______ ,KC_LBRC,_______,KC_RBRC),
+  S(KC_0), S(KC_0),   S(KC_SLSH),KC_CALC, KC_LCTL, KC_APP,  KC_LALT,                KC_SPC,  KC_SPC , KC_SPC,_______ ,KC_LBRC,KC_QUOTE,KC_RBRC),
 
   /* Keymap _ASYMBOLS: Alpha-Symbols Layer (holding right Fn from Alphas layer)
   */
@@ -126,9 +126,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {1, 1, HSV_RED}       
 );
-// Light LED 0 yellow when cursor keys are active
+// Light LED 0 yellow when non-cursor keys are active
 const rgblight_segment_t PROGMEM my_cursor_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 1, HSV_YELLOW}
+    {0, 1, HSV_WHITE}
 );
 // Light LED 2 and 3 white when symbols are active
 const rgblight_segment_t PROGMEM my_symbols_layer[] = RGBLIGHT_LAYER_SEGMENTS(
@@ -136,11 +136,11 @@ const rgblight_segment_t PROGMEM my_symbols_layer[] = RGBLIGHT_LAYER_SEGMENTS(
 );
 // Light LED 3 blue when numlock is active
 const rgblight_segment_t PROGMEM my_numlock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {3, 1, HSV_CYAN}
+    {3, 1, HSV_WHITE}
 );
-// Light LED 3 yellow when numlock is inactive
+// Light LED 3 off when numlock is inactive
 const rgblight_segment_t PROGMEM my_nonumlock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {3, 1, HSV_YELLOW}
+    {3, 1, HSV_OFF}
 );
 // Light LED 0,2 and 3 purple when function is active
 const rgblight_segment_t PROGMEM my_function_layer[] = RGBLIGHT_LAYER_SEGMENTS(
@@ -182,9 +182,9 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(5, layer_state_cmp(state, _FN));
+    rgblight_set_layer_state(5, layer_state_cmp(state, _FUNCTION));
     rgblight_set_layer_state(6, layer_state_cmp(state, _FD));
-    rgblight_set_layer_state(4, layer_state_cmp(state, _SYMBOLS) || layer_state_cmp(state, _ASYMBOLS));
-    rgblight_set_layer_state(3, layer_state_cmp(state, _BL) || layer_state_cmp(state, _ASYMBOLS));
+    rgblight_set_layer_state(4, (layer_state_cmp(state, _SYMBOLS) || layer_state_cmp(state, _ASYMBOLS)));
+    rgblight_set_layer_state(3, !(layer_state_cmp(state, _BL) || layer_state_cmp(state, _ASYMBOLS)));
     return state;
 }
