ced69abd51ee913ecb946799bb5825ed03c57e60
Andrew Richards <a.richards@computer.org>
2022-11-07 10:13:11 +0000

    Built G60 PCB with breathing LEDs (doesn't yet work)

--- keyboards/bioi/g60/config.h
+++ keyboards/bioi/g60/config.h
@@ -48,6 +48,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 /* Backlight Setup */
 #define BACKLIGHT_PIN B7
 #define BACKLIGHT_LEVELS 12
+#define BACKLIGHT_BREATHING
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
